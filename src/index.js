725cdc9240b613ea3d55554806d98a676c39ad56
Fábio Santos <fabiosantosart@gmail.com>
2016-05-05 10:40:15 +0100

    feat: initial implementation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Brightcove() {
+  return (
+    <div className="brightcove">
+    </div>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  Brightcove.propTypes = {
+  };
+}
